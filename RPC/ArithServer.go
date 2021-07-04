479cf413bbd769563fd3f6533e30bacb5ac24cb3
Vinny Meller <vinnymeller@gmail.com>
2021-07-04 12:47:25 -0500

    GOB RPC files

---
+++ RPC/ArithServer.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "net/rpc"
+    "net/http"
+)
+
+type Args struct {
+    A, B int
+}
+
+type Quotient struct {
+    Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+    *reply = args.A * args.B
+    return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+    quo.Quo = args.A / args.B
+    quo.Rem = args.A % args.B
+    return nil
+}
+
+func main() {
+    arith := new(Arith)
+    rpc.Register(arith)
+    rpc.HandleHTTP()
+
+    err := http.ListenAndServe(":1234", nil)
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+}
