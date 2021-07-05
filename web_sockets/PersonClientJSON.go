8cdfc9f92ef711420acf2963b19ed97efbfe5dfd
Vinny Meller <vinnymeller@gmail.com>
2021-07-04 21:53:04 -0500

    JSON web sockets

---
+++ web_sockets/PersonClientJSON.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "golang.org/x/net/websocket"
+    "fmt"
+    "os"
+)
+
+type Person struct {
+    Name string
+    Emails []string
+}
+
+func main() {
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: ", os.Args[0], "ws://host:port")
+        os.Exit(1)
+    }
+    service := os.Args[1]
+
+    conn, err := websocket.Dial(service, "", "http://localhost")
+    checkError(err)
+
+    person := Person{Name: "Vinny", Emails: []string{"vinnymeller@gmail.com", "vmeller@umich.edu"},}
+    
+    err = websocket.JSON.Send(conn, person)
+    if err != nil {
+        fmt.Println("Couldn't send msg " + err.Error())
+    }
+    os.Exit(0)
+}
+
+func checkError(err error) {
+    if err != nil {
+        fmt.Println("Fatal error ", err.Error())
+        os.Exit(1)
+    }
+}
